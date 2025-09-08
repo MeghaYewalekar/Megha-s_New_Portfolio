8095c03604ec1b519088802c483897a22953123f
MeghaYewalekar <myoct002@gmail.com>
2025-09-08 14:56:31 +0530

    some change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
-import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
-
+import { FaGithub,FaLinkedin } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className=" mb-20 flex items-center justify-between py-6">
-      <div className="w-10 h-7"><img className="w-full h-full object-contain" src="logo1" alt="" /></div>
+      <div className="w-30 h-20  "><img className="w-full h-full object-contain" src="logo2.png" /></div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a href="https://www.linkedin.com/in/megha-yewalekar-265b34244/">
           <FaLinkedin className="hover:text-zinc-400 cursor-pointer" />
