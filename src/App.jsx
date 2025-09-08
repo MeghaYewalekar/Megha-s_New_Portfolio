8462edb6fdb64141bc8c04d9035960a4a8d22ae5
MeghaYewalekar <myoct002@gmail.com>
2025-09-08 12:50:53 +0530

    some change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
-import Projects from "./components/projects";
+import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 
